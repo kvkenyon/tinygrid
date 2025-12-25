decfe867d1cfe9e6aa96395c412750ae6dc2c6b2
Kevin Kenyon <kevin@poweredbylight.com>
2025-12-25 10:32:50 -0600

    Remove unused ERCOTElectricalBus class

--- tinygrid/bus/ercot.py
+++ tinygrid/bus/ercot.py
@@ -3,21 +3,6 @@
 import csv
 from pathlib import Path
 
-from attrs import define, field
-
-
-@define
-class ERCOTElectricalBus:
-    """ERCOT Electrical Bus"""
-
-    bus_name: str = field(default="")
-    bus_id: str = field(default="")
-    bus_type: str = field(default="")
-    bus_location: str = field(default="")
-    bus_status: str = field(default="")
-    bus_created_at: str = field(default="")
-
-
 # Path to the CSV file
 _CSV_PATH = Path(__file__).parent / "ercot_electrical_bus_to_hub_map.csv"
 
